b09c5226a5e05815f3c1aaf0149e0df101a2a13b
Sebastian Castillo <tantrum5535@gmail.com>
2023-12-27 10:42:17 -0300

    creando el listado de pacientes

---
+++ src/Components/Paciente.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function () {
+    return (
+        <div className='m-3 bg-white shadow-md px-5 py-10 rounded-xl'>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Nombre: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Propietario: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Fecha Alta: {''}
+                <span className='font-normal normal-case'>Hook</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Email: {''}
+                <span className='font-normal normal-case'>email@email.com</span>
+            </p>
+            <p className='font-bold mb-3 text-gray-700 uppercase'>
+                Síntomas: {''}
+                <span className='font-normal normal-case'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci modi unde aliquam labore voluptatem voluptates saepe delectus nobis ullam eaque? Deleniti ipsam sit incidunt eaque atque dolorum vitae tempora. Sapiente!</span>
+            </p>
+        </div>
+    )
+}

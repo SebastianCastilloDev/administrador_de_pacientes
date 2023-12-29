13123f5579befde29550148aa5fe190d7109de62
Sebastian Castillo <tantrum5535@gmail.com>
2023-12-29 15:48:39 -0300

    leyendo inputs y agregandolos al state

--- src/Components/Formulario.jsx
+++ src/Components/Formulario.jsx
@@ -4,7 +4,17 @@ import { useState, useEffect } from "react"
 export default function Formulario() {
 
     const [nombre, setNombre] = useState('')
+    const [propietario, setPropietario] = useState('')
+    const [email, setEmail] = useState('')
+    const [fecha, setFecha] = useState('')
+    const [sintomas, setSintomas] = useState('')
 
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('enviando formulario')
+    }
 
     return (
         <div className="md:w-1/2 lg:w-2/5">
@@ -16,7 +26,10 @@ export default function Formulario() {
                 <span className="text-indigo-600 font-bold">Administralos</span>
             </p>
 
-            <form className="bg-white shadow rounded-lg py-10 px-5 mb-10">
+            <form
+                className="bg-white shadow rounded-lg py-10 px-5 mb-10 mx-5"
+                onSubmit={handleSubmit}
+            >
                 <div className="mb-5">
                     <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
                         Nombre Mascota
@@ -39,6 +52,8 @@ export default function Formulario() {
                         id="propietario"
                         placeholder="Nombre propietario"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={propietario}
+                        onChange={e => setPropietario(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -50,6 +65,20 @@ export default function Formulario() {
                         id="email"
                         placeholder="Email propietario"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </div>
+                <div className="mb-5">
+                    <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">
+                        Email Propietario
+                    </label>
+                    <input
+                        type="date"
+                        id="alta"
+                        className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                        value={fecha}
+                        onChange={e => setFecha(e.target.value)}
                     />
                 </div>
                 <div className="mb-5">
@@ -60,6 +89,8 @@ export default function Formulario() {
                         id="sintomas"
                         className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
                         placeholder="Describe los síntomas"
+                        value={sintomas}
+                        onChange={e => setSintomas(e.target.value)}
                     />
                 </div>
                 <button

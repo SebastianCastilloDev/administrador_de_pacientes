3ec5d20f201f9b29e432564935832bb44dcf7a37
Sebastian Castillo <tantrum5535@gmail.com>
2023-12-30 14:53:36 -0300

    mostrando un texto condicional si no hay pacientes

--- src/Components/ListadoPacientes.jsx
+++ src/Components/ListadoPacientes.jsx
@@ -1,31 +1,39 @@
 import Paciente from './Paciente'
 
 export default function ListadoPacientes({ pacientes }) {
-    const id =
-        console.log(pacientes)
+
     return (
         <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll'>
-            <h2 className='font-black text-3xl text-center'>
-                Listado Pacientes
-            </h2>
-            <p className='text-xl mt-5 mb-10 text-center'>
-                Administra tus {''}
-                <span className='text-indigo-600 font-bold'>Pacientes y citas</span>
-            </p>
+            {pacientes?.length ? (
+                <>
+                    <h2 className='font-black text-3xl text-center'>Listado Pacientes</h2>
+                    <p className='text-xl mt-5 mb-10 text-center'>
+                        Administra tus {''}
+                        <span className='text-indigo-600 font-bold'>Pacientes y citas</span>
+                    </p>
 
-            {
-                pacientes.map(paciente => {
-                    return (
-                        <Paciente
-                            key={paciente.key}
-                            paciente={paciente}
-                        />
-                    )
-                })
+                    {
+                        pacientes.map(paciente => {
+                            return (
+                                <Paciente
+                                    key={paciente.key}
+                                    paciente={paciente}
+                                />
+                            )
+                        })
+                    }
+                </>
+            )
+                :
+                (
+                    <>
+                        <h2 className='font-black text-3xl text-center'>No hay pacientes</h2>
+                        <p className='text-xl mt-5 mb-10 text-center'>
+                            Comienza creando un paciente
+                        </p>
+                    </>
+                )
             }
-
-
-
 
         </div>
     )
